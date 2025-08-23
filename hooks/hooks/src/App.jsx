18026f8653cfd94f930ecf02c8d21187656cc393
uuuuday <krishnauday200301@gmail.com>
2025-08-23 15:53:47 +0530

    add the info about imp interview question

--- hooks/hooks/src/App.jsx
+++ hooks/hooks/src/App.jsx
@@ -8,8 +8,30 @@ function App() {
 
 
   function addvalue(){
-    counter = counter + 1;
-    setCounter(counter)
+  
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+
+
+    //now the questions is what will be teh value of counter?
+    //same? no change ?
+    //find out 
+    //answer is react always sned the teh value sin batches 
+    //so it will presume as asame as value 
+
+
+    setCounter(precounter => precounter +1)
+    setCounter(precounter => precounter +1)
+    setCounter(precounter => precounter +1)
+    setCounter(precounter => precounter +1)
+    //setcounter gives us call back value //using this we can access prev state
+
+
+    //this gives us state of previous valu e
+    //now it would incraements of 4
   }
 
   function removeValue(){
