8d59892cfbe818ef7f13f6c17b252cd033ad125c
uuuuday <krishnauday200301@gmail.com>
2025-08-26 10:53:42 +0530

    add the logic code in the app.jsx file

--- contextLocal/README.md
+++ contextLocal/README.md
@@ -4,4 +4,6 @@
 2)u define the initial states in craete context wla object !!!!remmber we give only the functionality name to the object not the defination of the functionality 
 
 
-3)we export all craete context ke functions to index.js os that its easy for us to export teh functions
+3)we export all craete context ke functions to index.js os that its easy for us to export the functions
+
+4)now we rae going for setting teh up the logic of provider in the in the app.jsx layout 
--- contextLocal/src/App.jsx
+++ contextLocal/src/App.jsx
@@ -1,7 +1,62 @@
-export default function App() {
+
+import './App.css'
+import { TodoProvider } from './context'
+
+
+function App() {
+      //now we have to write all the business logic here 
+      //firest we define a ste for for etodos 
+      const [todos ,setTodos] = useState([])
+
+
+      const addToDo = (todo) => {
+          setTodos((prev)=>[{id:Date.now(),...todo},...prev])
+      }
+      
+
+      const deleteTodo = (id) => {
+          setTodos((prev)=> prev.filter((todo) => todo.id !== id))
+      }
+
+
+        const updateTodo = (id, todo) => {
+      setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))   
+        }
+
+
+        const tooglecomplete = (id) => {
+        //console.log(id);
+      setTodos((prev) => 
+      prev.map((prevTodo) => 
+       prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
+
+
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <TodoProvider value={{todos,addToDo,deleteTodo,updateTodo,tooglecomplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   )
 }
+
+export default App
